98d113dbfd7c8fdb654fba80a265f69d129464ce
lazarusmugo <lazarusm@kabarak.ac.ke>
2022-10-19 15:50:44 -0700

    5-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main  Prints all single digits of base 10 from 
+ * Return: Always 0
+ */
+int main(void)
+{
+		int num;
+
+			for (num = 0; num < 10; num++)
+						putchar((num % 10) + '0');
+
+				putchar('\n');
+
+					return (0);
+}
