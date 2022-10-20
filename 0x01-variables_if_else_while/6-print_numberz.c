a22b532283a999f7c6466f8628cf6d1af6c13ae1
lazarusmugo <lazarusm@kabarak.ac.ke>
2022-10-19 17:13:16 -0700

    d

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,12 +5,9 @@
  */
 int main(void)
 {
-		int num;
-
+int num;
 for (num = 0; num < 10; num++)
-				putchar((num % 10) + '0');
-
-				putchar('\n');
-
-					return (0);
+putchar((num % 10) + '0');
+putchar('\n');
+return (0);
 }
