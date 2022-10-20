552b8c2c5ab246faa9dd1fd70932e070309b7129
lazarusmugo <lazarusm@kabarak.ac.ke>
2022-10-19 17:16:35 -0700

    d

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,16 +5,12 @@
  */
 int main(void)
 {
-		int num;
-			char letter;
-
-				for (num = 0; num < 10; num++)
-							putchar((num % 10) + '0');
-
-					for (letter = 'a'; letter <= 'f'; letter++)
-								putchar(letter);
-
-						putchar('\n');
-
-							return (0);
+int num;
+char letter;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
 }
