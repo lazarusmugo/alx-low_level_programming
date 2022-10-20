bd46e9d493450ac91a9a19c58fbbe586cd4c260c
lazarusmugo <lazarusm@kabarak.ac.ke>
2022-10-19 17:02:07 -0700

    s

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 /**
 * main - Prints alphabets in lowercase esxcept q and e
-*Return : Always 0.
+* Return : Always 0.
 */
 int main(void)
 {
 char letter;
-for (letter = 'a'; letter <='z'; letter++)
-{
+for (letter = 'a'; letter <= 'z'; letter++)
+{i
 if (letter != 'e' && letter != 'q')
 	putchar(letter);
 }
