c8cf98a608c6cb909615fe5284eab58a03eb51aa
lazarusmugo <lazarusm@kabarak.ac.ke>
2022-10-23 07:50:57 -0700

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,16 @@
-include "main.h"                                                
-/**                                                              * _isdigit - checks for a digit                                  
-*@c: input                                                       * Return: 1 if c is a digit, 0 otherwise                         
-*/                                                               
-int _isdigit(int c)                                                  {                                                                
-       if ((c >= 48) && (c <= 57))            			                return (1);                                      
-	        else                                                              return (0);                                      } 
+#include "main.h"
+/**
+ * _isdigit - checks fr two digits
+ * @c: input
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+
+{
+
+		if ((c >= 48) && (c <= 57))
+
+					return (1);
+			else
+						return (0);
+}
