e545132caa5128f997795521f53f828cc03ef1bc
lazarusmugo <lazarusm@kabarak.ac.ke>
2022-10-23 07:06:10 -0700

    f

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H                                                   
-#define MAIN_H                                                     
+#ifndef MAIN_H
+#define MAIN_H                                                   
                                       
 int _putchar(char c);                                            
 int _isupper(int c);                                             
@@ -12,7 +12,6 @@ void print_line(int n);
 void print_diagonal(int n);                                      
 void print_square(int size);
 void print_triangle(int size);
-                                                         
-#endif                                                           
 
+#endif                                                          
 ~      
