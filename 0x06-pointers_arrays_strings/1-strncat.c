18ba41db19dd54bb9e036dca2b62478a254e320a
lazarusmugo <lazzohchainz@gmail.com>
2023-07-08 03:24:26 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat -Cancatenates strings
+ * @dest: destination string
+ * @src: source string
+ * @n: limit of concatnation
+ *
+ * Return: Pointer to destination
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int dlen = 0, j = 0;
+
+while (dest[dlen])
+{
+	dlen++;
+}
+
+while (j < n && src[j])
+{
+dest[dlen] = src[j]
+	dlen++;
+j++;
+}
+dest[dlen + n + 1] = '\0';
+
+return (dest);
+}
+
