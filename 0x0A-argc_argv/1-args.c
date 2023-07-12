708aa5fbf2925c9aeed772c6be1f35bd6c4af910
lazarusmugo <lazzohchainz@gmail.com>
2023-07-12 02:35:12 -0700

    1 task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - number of argumnts
+ * @argc: numbr of arguments
+ * @argv: array of argumnts
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc);
+return (0);
+}
+
