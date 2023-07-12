275ba3e7195c7217337f245b08adb65508339ada
lazarusmugo <lazzohchainz@gmail.com>
2023-07-12 03:29:03 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,9 +18,12 @@ int main (int argc, char *argv[])
 		a = atoi(argv[1]);
 		b = atoi(argv[2]);
 		printf("%d\n", a * b);
-		return (0);
-	}
+			}
+	else
+			{
 	printf("Error\n");
 	return (1);
+			}
+	return (0);
 }
 
